310ad6320a1d2343660b8cf7f76f58e57f9e42b1
kalyanaalk <kanza.putri@yahoo.com>
2024-10-06 19:20:35 +0700

    feat: add dockerized hello world app

---
+++ C. Pemrograman Go Lanjut/C.35. Dockerize Aplikasi Golang/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+	   komentar kode
+	   menampilkan pesan hello world
+	*/
+	for i := 0; i < 100; i++ {
+		fmt.Println("hello world")
+	}
+
+	// fmt.Println("baris ini tidak akan dieksekusi")
+}
